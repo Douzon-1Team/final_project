80699afa20a95ef24ef35b42eeafa0a972854d60
LJY <ll55kk678@gmail.com>
2022-06-13 21:57:23 +0900

    feat/jwt : 로그인 기능 backend 구현

---
+++ src/main/java/com/example/final_project/dto/LoginRequestDto.java
@@ -0,0 +1,16 @@
+package com.example.final_project.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequestDto {
+    private String empno;
+    private String password;
+}
---
+++ src/main/java/com/example/final_project/model/Role.java
@@ -0,0 +1,5 @@
+package com.example.final_project.model;
+
+public enum Role {
+    ROLE_ADMIN, ROLE_MANAGER, ROLE_USER
+}
