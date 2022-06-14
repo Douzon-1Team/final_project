1c2e76809c8ca8452e091d07a8c851a0ca4fe859
Quence1151 <dlgksdyd1151@naver.com>
2022-06-14 19:54:47 +0900

    근무시간 표시 컴포넌트 구현

--- src/main/java/com/example/final_project/FinalProjectApplication.java
+++ src/main/java/com/example/final_project/FinalProjectApplication.java
@@ -1,10 +1,12 @@
 package com.example.final_project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan(value = {"com.example.final_project.mapper"})
 @SpringBootApplication
+@MapperScan(value = {"com.example.final_project.mapper"})
 public class FinalProjectApplication {
 
     public static void main(String[] args) {
