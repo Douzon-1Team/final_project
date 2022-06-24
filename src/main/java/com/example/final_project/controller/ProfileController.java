1da3ded7099490aeb8698da9ca2f7e6f39c730f8
2jelee <2ttobokk@gmail.com>
2022-06-24 19:30:20 +0900

    feat/profile : ProfileController 이미지 업로드

--- src/main/java/com/example/final_project/controller/ProfileController.java
+++ src/main/java/com/example/final_project/controller/ProfileController.java
@@ -1,10 +1,17 @@
 package com.example.final_project.controller;
 
 import com.example.final_project.dto.EmpInfoDto;
+import com.example.final_project.dto.EmpUpdateDto;
 import com.example.final_project.service.ProfileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.ServletException;
+import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +25,25 @@ public class ProfileController {
         return ResponseEntity.ok().body(empInfoDto);
     }
 
-//    @PostMapping("/profile/update/{empno}")
-//    public ResponseEntity<EmpInfoDto> updateProfile(@RequestBody EmpInfoDto empInfoDto){
-//        profileService.updateProfile(empInfoDto);
-//        return ResponseEntity.ok().body(empInfoDto);
+//    @PostMapping("/profile/update")
+//    public ResponseEntity updateProfile(@RequestPart(value = "EmpUpdateDto") EmpUpdateDto updateDto,
+//                                        @RequestPart(value = "file", required = false) MultipartFile profile){
+//        System.out.println(updateDto);
+//        profileService.updateProfile(updateDto, profile);
+//        return ResponseEntity.ok().build();
 //    }
+
+//    @PostMapping("/profile/update")
+//    public ResponseEntity updateProfile(@RequestBody Object profile){
+//        System.out.println(profile);
+////        profileService.updateProfile(updateDto, profile);
+//        return ResponseEntity.ok().build();
+//    }
+
+    @PostMapping(path = "/profile/update")
+    public String updateProfile(@RequestBody HashMap<String, Object> param){
+        System.out.println(param.get("image"));
+        System.out.println(param);
+        return param.get("image").toString();
+    }
 }
--- src/main/java/com/example/final_project/service/ProfileService.java
+++ src/main/java/com/example/final_project/service/ProfileService.java
@@ -1,22 +1,34 @@
 package com.example.final_project.service;
 
 import com.example.final_project.dto.EmpInfoDto;
+import com.example.final_project.dto.EmpUpdateDto;
+import com.example.final_project.exception.EmpException;
+import com.example.final_project.exception.ErrorCode;
+import com.example.final_project.exception.PasswordException;
+import com.example.final_project.mapper.AdminMapper;
 import com.example.final_project.mapper.DeptMapper;
 import com.example.final_project.mapper.EmpInfoCompMapper;
 import com.example.final_project.mapper.EmployeeMapper;
 import com.example.final_project.model.EmpInfoComp;
 import com.example.final_project.model.Employee;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class ProfileService {
 
-    private final EmployeeMapper employeeMapper;
     private final EmpInfoCompMapper empInfoCompMapper;
+    private final EmployeeMapper employeeMapper;
     private final DeptMapper deptMapper;
+    private final AdminMapper adminMapper;
+    private final QRService qrService;
+    private final S3Service s3Service;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public EmpInfoDto getProfile(String empno){
@@ -32,5 +44,46 @@ public class ProfileService {
                                     .profile(null)
                                     .role(employee.getRole())
                                     .build();
+    }
+
+    @Transactional
+    public void updateProfile(@RequestPart(value = "EmpUpdateDto") EmpUpdateDto updateDto,
+                       @RequestPart(value = "profile") MultipartFile profile){
+//    public void updateProfile(EmpUpdateDto updateDto, MultipartFile profile){
+        employeeMapper.findByUserId(updateDto.getEmpno())
+                .orElseThrow(() -> new EmpException(ErrorCode.EMP_NOTFOUND));
+
+        String profileUrl = null;
+        if(profile != null)
+            profileUrl = s3Service.uploadProfile(profile, updateDto.getEmpno());
+
+        String password = null;
+        if(updateDto.getPwd() != null) {
+            validatePassword(updateDto.getEmpno(), updateDto.getPwd(), updateDto.getNewPwd(), updateDto.getChkPwd());
+            password = passwordEncoder.encode(updateDto.getNewPwd());
+        }
+
+//        employeeMapper.updateByEmpno(EmpUpdateDto.toEmployee(updateDto, passwordEncoder.encode(password), profileUrl));
+//        String deptNo = deptMapper.findByDeptName(updateDto.getDeptName());
+//        empInfoCompMapper.updateByEmpno(EmpUpdateDto.toEmpInfoComp(updateDto, deptNo));
+        employeeMapper.updateByEmpno(EmpUpdateDto.toEmployee(updateDto, password, profileUrl));
+        String deptNo = deptMapper.findByDeptName(updateDto.getDeptName());
+        empInfoCompMapper.updateByEmpno(EmpUpdateDto.toEmpInfoComp(updateDto, deptNo));
+    }
+
+    public void validatePassword(String empno, String pwd, String newPwd, String chkPwd){
+        String originPwd = employeeMapper.findPasswordByEmpno(empno)
+                .orElseThrow(() -> new EmpException(ErrorCode.EMP_NOTFOUND));
+
+        if(!passwordEncoder.matches(pwd, originPwd)) {
+            System.out.println(passwordEncoder.encode(pwd));
+            System.out.println(originPwd);
+            throw new PasswordException(ErrorCode.WRONG_PASSWORD);
+        }
+        if(!newPwd.equals(chkPwd))
+            throw new PasswordException(ErrorCode.MISMATCH_PASSWORD);
+
+        if(pwd.equals(newPwd))
+            throw new PasswordException(ErrorCode.SAME_PASSWORD);
     }
 }
