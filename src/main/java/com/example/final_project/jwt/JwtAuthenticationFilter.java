a34203df416002feed5603d9cf1611e2e728bb80
Limitiz <ll55kk678@gmail.com>
2022-07-05 16:58:14 +0900

    fix/jwt : 로그아웃시 쿠키 삭제

--- src/main/java/com/example/final_project/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/final_project/jwt/JwtAuthenticationFilter.java
@@ -19,25 +19,20 @@ import lombok.RequiredArgsConstructor;
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
     private final JwtTokenProvider jwtTokenProvider;
-    public static final String AUTHORIZATION_HEADER = "Authorization";
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // 헤더에서 JWT 를 받아오기
         String token = resolveToken((HttpServletRequest) request);
-        // 유효한 토큰인지 확인
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            // 토큰이 유효하면 토큰으로부터 유저 정보를 받아오기
+
+        if (token != null && jwtTokenProvider.validateToken(token, request)) {
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            // SecurityContext 에 Authentication 객체를 저장
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
         chain.doFilter(request, response);
     }
 
-    // Request의 Header에서 token 값 가져오기    "Authorization" : "token value"
-    public String resolveToken(HttpServletRequest request) {  // HttpServletRequest 라이브러리 추가할것
-        return request.getHeader(AUTHORIZATION_HEADER);
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("Authorization");
     }
 
 }
