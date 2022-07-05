2c284ed2360107e1662a2fcb13bf0a6c555101e1
sjoongh <az45687@naver.com>
2022-07-05 22:40:11 +0900

    fixing app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,21 +26,6 @@ const RootStyle = styled('div')({
     overflow: 'hidden'
 });
 
-const APP_BAR_MOBILE = 64;
-const APP_BAR_DESKTOP = 92;
-
-const MainStyle = styled('div')(({ theme }) => ({
-    flexGrow: 1,
-    overflow: 'auto',
-    minHeight: '100%',
-    paddingTop: APP_BAR_MOBILE + 24,
-    paddingBottom: theme.spacing(10),
-    [theme.breakpoints.up('lg')]: {
-        paddingTop: APP_BAR_DESKTOP + 24,
-        paddingLeft: theme.spacing(2),
-        paddingRight: theme.spacing(2)
-    }
-}));
 function App() {
     const empRole = useSelector( (state) => state.EMP_INFO.empInfo[2] );
     let LoginChk = localStorage.getItem("LoginChk");
