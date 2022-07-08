649f07d639ada31eb64908754788611ef4884d35
Quence1151 <dlgksdyd1151@naver.com>
2022-07-08 15:11:54 +0900

    fix: css파일 누락

---
+++ frontend/src/styles/NotificationStyle.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const ListImg = styled.div`
+    min-width: 56px;
+    flex-shrink: 0;
+`;
+
+const MemberImg = styled.img`
+    width: 45px;
+    height: 45px; 
+    border-radius: 23px;
+`;
+
+export {MemberImg, ListImg};
