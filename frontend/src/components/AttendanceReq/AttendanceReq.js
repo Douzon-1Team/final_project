e0a1a6ddb387580cc6ccd3ed302c774eddda846b
seungbeom613 <jsw0613a@gmail.com>
2022-07-10 01:09:07 +0900

    작업 ã완료

--- frontend/src/components/AttendanceReq/AttendanceReq.js
+++ frontend/src/components/AttendanceReq/AttendanceReq.js
@@ -7,30 +7,34 @@ import dayjs from "dayjs";
 import {useNavigate} from "react-router-dom";
 import {modalStyle} from "../common/Modal/ModalStyle";
 import axios from "axios";
-import { useLocation } from "react-router";
-import { useSelector } from "react-redux";
+import {useLocation} from "react-router";
+import {useSelector} from "react-redux";
 import {MainStyle} from "../../styles/Globalstyle"
+import {BsArrowReturnRight} from "react-icons/bs";
 
 export const AttendanceReq = () => {
-    const {state} = useLocation(); // TODO : 달력으로부터 넘어온 데이터
+    const {state} = useLocation();
     const selectedDate = (state == null) ? null : state._date;
     const empName = useSelector((state) => state.EMP_INFO.empInfo[1]);
     const empNo = useSelector((state) => state.EMP_INFO.empInfo[0]);
     const [loadingData, setLoadingData] = useState(true);
     const [startTime, setStartTime] = useState(9);
     const [endTime, setEndTime] = useState(18);
+    const [acceptShow, setAcceptShow] = useState(false);
+
     useEffect(() => {
         async function getData() {
             await axios
                 .get("http://localhost:8080/getvacationdata", {params: {'empNo': empNo}})
                 .then((res) => {
-                    console.log(res.data)
                     setLoadingData(false);
-                    setStartTime(res.data[0].workStart.substr(0, 2));
-                    setEndTime(res.data[0].workEnd.substr(0, 2));
-                })
-                .catch((res) => {
-                    console.log("수신실패 : ", res);
+                    if (res.data[0].flex == 0) {
+                        setStartTime(res.data[0].workStart.substr(0, 2));
+                        setEndTime(res.data[0].workEnd.substr(0, 2));
+                    } else if (res.data[0].flex == 1) {
+                        setStartTime(res.data[0].workStartf.substr(0, 2));
+                        setEndTime(res.data[0].workEndf.substr(0, 2));
+                    }
                 })
         }
 
@@ -38,44 +42,99 @@ export const AttendanceReq = () => {
             getData();
         }
     }, [])
+
     const f1 = () => {
         axios
             .post("/vacationreq2", {
                 empNo: empNo,
                 req: req,
-                startFormat: startFormat,
+                startFormat: startFormat2,
                 endFormat: endFormat,
                 comment: comment,
             })
             .then((response) => {
-                console.log("전달완료 무야호");
             })
-            .catch((error) => {
-                console.log(error, "*******************************");
-            });
     };
-    // -------------------------------------------
-    const [sortNum, setSortNum] = useState(0); // 근태구분
+
+    const [sortNum, setSortNum] = useState(0);
     const today = new Date();
-    const [startDate, setStartDate] = useState(state == null ? today : new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate(), startTime, 0, 0)); // 근태조정 시작 시간
-    const [endDate, setEndDate] = useState(state == null ? today : new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate(), endTime, 0, 0)); // 근태조정 시작 시간
-    // -------------------------------------------
+    const [startDate, setStartDate] = useState(state == null ? today : new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate(), startTime, 0, 0));
+    const [endDate, setEndDate] = useState(state == null ? today : new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate(), endTime, 0, 0));
+
     const [modalSwitch, setModalSwitch] = useState(false);
     const [sendBefore, setSendBefore] = useState(false);
     const [sendAfter, setSendAfter] = useState(false);
     const [req, setReq] = useState("");
-    // -------------------------------------------
+    const start = dayjs(startDate);
+    const end = dayjs(endDate)
+    let startFormat;
+    startFormat = start.format("YYYY-MM-DD");
+
+    let startFormat2;
+    startFormat2 = start.format("YYYY-MM-DD HH:00:00");
+    let endFormat;
+    endFormat = end.format("YYYY-MM-DD HH:00:00");
+    const [loadingDate, setLoadingDate] = useState(true);
+    const [status, setStatus] = useState("수정할 데이터 없음");
+    const [statusColor, setStatusColor] = useState(false);
+    const [attstatid, setAttstatid] = useState(null);
+    useEffect(() => {
+        async function getDate() {
+            await axios
+                .get("http://localhost:8080/gettargetdate", {params: {'empNo': empNo, 'date': startFormat}})
+                .then((res) => {
+                    setAttstatid(res.data[0].attstatid);
+                    if (((res.data[0].etc == "지각") || (res.data[0].etc == "결근") || (res.data[0].etc == "출근미등록") || (res.data[0].etc == "퇴근미등록")) && (req == res.data[0].etc)) {
+                        if (res.data[0].agree == 1) {
+                            setStatusColor(false);
+                            setStatus("근태이상 (처리됨)");
+                        } else {
+                            setStatusColor(true);
+                            setStatus("근태이상 (" + req + ")");
+                        }
+                    } else {
+                        setStatus("수정할 데이터 없음")
+                        setStatusColor(false);
+                    }
+                })
+        }
+
+        if (loadingDate) {
+            getDate();
+        }
+    }, [startFormat, req])
+
+    async function acceptAttendance() {
+        await axios
+            .post("http://localhost:8080/acceptatt", {
+                'attstatid': attstatid,
+                'empNo': empNo,
+            })
+            .then((res) => {
+                setStatus("근태이상 (처리됨)");
+                setStatusColor(false);
+            })
+    }
+
     function onSet(para) {
-        if (para === 1) setReq("지각");
-        else if (para === 2) setReq("조퇴");
-        else if (para == 3) setReq("결근");
-        else if (para == 4) setReq("출근미등록");
-        else if (para == 5) setReq("퇴근미등록");
+        if (para === 1) {
+            setReq("지각");
+        } else if (para == 3) {
+            setReq("결근");
+        } else if (para == 4) {
+            setReq("출근미등록");
+        } else if (para == 5) {
+            setReq("퇴근미등록");
+        }
 
         if (sortNum === para) {
             setSortNum(0);
+            setAcceptShow(false);
+            setReq("");
         } else {
+            setLoadingDate(true);
             setSortNum(para);
+            setAcceptShow(true);
         }
     }
 
@@ -89,33 +148,27 @@ export const AttendanceReq = () => {
         navigate("/main");
     }
 
-    const start = dayjs(startDate);
-    const end = dayjs(endDate);
-    let startFormat, endFormat;
-    startFormat = start.format("YYYY-MM-DD HH:mm:00");
-    endFormat = end.format("YYYY-MM-DD HH:mm:00");
-    // ----------------------------------------------------------
     const [comment, setComment] = useState("");
     const handleChangeComment = (e) => {
         setComment(e.target.value);
     };
 
-    // ---------------------------------------------------------------------------------
     function sendData(sortNum) {
-        if (sortNum === 0) {
-            setModalSwitch(true);
-        } else {
-            setSendBefore(true);
+        if (statusColor == true) {
+            if (sortNum === 0) {
+                setModalSwitch(true);
+            } else {
+                setSendBefore(true);
+            }
         }
     }
 
-    // ---------------------------------------------------------------------------------
     let navigate = useNavigate();
-    // ----------------------------------------------------------
 
     return (
-        <>
+        <MainStyle>
             <Container>
+                <Title> 근태조정 신청서 </Title>
                 {modalSwitch && (
                     <Modal>
                         <ModalWindow>
@@ -163,9 +216,6 @@ export const AttendanceReq = () => {
                         <ButtonA onClick={() => onSet(1)} sortNum={sortNum}>
                             지각
                         </ButtonA>
-                        <ButtonB onClick={() => onSet(2)} sortNum={sortNum}>
-                            조퇴
-                        </ButtonB>
                         <ButtonC onClick={() => onSet(3)} sortNum={sortNum}>
                             결근
                         </ButtonC>
@@ -189,12 +239,22 @@ export const AttendanceReq = () => {
                                 selected={startDate}
                                 onChange={(date) => {
                                     setStartDate(date);
+                                    setEndDate(date);
                                 }}
-                                maxDate={startDate}
+                                maxDate={today}
                             />
                         </TermSelect>
                     </TermContent>
                 </LeaveTerm>
+                {/* ------------------------- */}
+                <AttendanceInfo acceptShow={acceptShow}>
+                    <InfoCon>
+                        <Text1> <BsArrowReturnRight/> 상태 : </Text1>
+                        <ConditionInfo statusColor={statusColor}>{status}</ConditionInfo>
+                        <Text1> 본인 확인 : </Text1>
+                        <AcceptButton statusColor={statusColor} onClick={() => acceptAttendance()}>확 인</AcceptButton>
+                    </InfoCon>
+                </AttendanceInfo>
                 {/* ------------------------- */}
                 <LeaveName>
                     <NameTag>대상자</NameTag>
@@ -202,21 +262,22 @@ export const AttendanceReq = () => {
                 </LeaveName>
                 {/* ------------------------- */}
                 <LeaveReason>
-                    <ReasonTag>사유</ReasonTag>
+                    <ReasonTag>사 유</ReasonTag>
                     <ReasonContent name="comment" onChange={handleChangeComment}/>
                 </LeaveReason>
                 {/* ------------------------- */}
                 <ButtonBox>
-                    <Button2_1 onClick={() => sendData(sortNum)}>신 청</Button2_1>
+                    <Button2_1 statusColor={statusColor} onClick={() => sendData(sortNum)}>신 청</Button2_1>
                     <Button2_2 onClick={() => navigate("/main")}>취 소</Button2_2>
                 </ButtonBox>
             </Container>
-        </>
+        </MainStyle>
     );
 };
 
 const {
     Container,
+    Title,
     LeaveSort,
     LeaveTerm,
     LeaveName,
@@ -233,11 +294,15 @@ const {
     Button2_1,
     Button2_2,
     ButtonA,
-    ButtonB,
     ButtonC,
     ButtonD,
     ButtonE,
     TermSelect,
+    AttendanceInfo,
+    ConditionInfo,
+    AcceptButton,
+    Text1,
+    InfoCon,
 } = style;
 
 const {Modal, ModalWindow, ModalTitle, YesButton, NoButton} = modalStyle;
